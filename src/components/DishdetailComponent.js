fd76d9f281151b12c52eb870da17556a37ced85a
Abhijith-tee <63539211+Abhijith-tee@users.noreply.github.com>
2020-05-21 01:41:27 +0530

    assignment week1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Form } from 'reactstrap';
+
+
+class Dishdetail extends Component{
+
+    render() {
+        const commentList = this.props.dish.comments.map(item => (
+            <li>
+              {item.comment}
+              <br/><br/>
+              -- {item.author},  {item.date}
+              <br/><br/>
+            </li>
+          )
+        );
+        return (
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              <Card>
+                <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                <CardBody>
+                  <CardTitle>{this.props.dish.name}</CardTitle>
+                  <CardText>{this.props.dish.description}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1">
+              <ul>{commentList}</ul>
+            </div>
+          </div>
+        );
+      }
+
+}
+
+
+
+export default Dishdetail;
